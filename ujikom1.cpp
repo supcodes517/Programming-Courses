6a2818e8d12427a8ac3e1667b931f64c512a6ff9
supcodes517 <36922712+supcodes517@users.noreply.github.com>
2025-10-29 04:22:14 +0000

    tugas ujikom fix

---
+++ ujikom1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(int i=0; i<n;i++){
+        cout<<i<<" ";
+    }
+    return 0;   
+}
